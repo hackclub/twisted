6459d19cf362fd00d75d0d48c1be44c51c218bef
KavyanshKhaitan2 <kavyanshkhaitan11@gmail.com>
2026-09-08 22:37:59 +0530

    Change date to 7/9

--- twisted/twisted_site/hackatime.py
+++ twisted/twisted_site/hackatime.py
@@ -48,7 +48,7 @@ def me(access_token) -> MeResponse:
 def projects(
     access_token,
     include_archived=False,
-    start: datetime | None = datetime(2026, 9, 8, tzinfo=UTC),
+    start: datetime | None = datetime(2026, 9, 7, tzinfo=UTC),
     projects: list[str] | None = None,
 ) -> list[HackatimeProject]:
     """Returns the user's projects with time totals."""
