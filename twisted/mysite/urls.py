18e81af1c62f86d13d0207e2d631d6e24a058ecf
Lewin Kelly <heli@odex.cf>
2026-09-09 13:32:39 -0400

    List spreading instead of concatenation

--- twisted/mysite/urls.py
+++ twisted/mysite/urls.py
@@ -21,9 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
 
-urlpatterns = [
-    path("", include("twisted_site.urls")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns = [path("", include("twisted_site.urls")), *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)]
 
 if settings.DEBUG:
     urlpatterns += [
