375741c15109b69d30284ce3fde0f4e66a65642d
Lewin Kelly <heli@odex.cf>
2026-09-08 20:29:12 -0400

    Catch error upon failing to log to Slack channel

--- twisted/twisted_site/slack.py
+++ twisted/twisted_site/slack.py
@@ -1,13 +1,17 @@
+import logging
 from typing import Any
 
 from django.conf import settings
 from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 from slack_sdk.web.slack_response import SlackResponse
 
 SLACK_TOKEN = settings.SLACK_TOKEN
 SLACK_LOG_CHANNEL = settings.SLACK_LOG_CHANNEL
 
 slack_bot = WebClient(token=SLACK_TOKEN)
+
+logger = logging.getLogger(__name__)
 
 
 def send_blocks(
@@ -23,9 +27,12 @@ def send_blocks(
 
 
 def log_to_channel(message: str) -> None:
-    if settings.DEBUG:
-        _ = slack_bot.chat_postMessage(
-            channel=SLACK_LOG_CHANNEL, text=message, username="[DEBUG]"
-        )
-    else:
-        _ = slack_bot.chat_postMessage(channel=SLACK_LOG_CHANNEL, text=message)
+    try:
+        if settings.DEBUG:
+            _ = slack_bot.chat_postMessage(
+                channel=SLACK_LOG_CHANNEL, text=message, username="[DEBUG]"
+            )
+        else:
+            _ = slack_bot.chat_postMessage(channel=SLACK_LOG_CHANNEL, text=message)
+    except SlackApiError:
+        logger.exception(f"Failed to log to Slack channel, message {message}")
