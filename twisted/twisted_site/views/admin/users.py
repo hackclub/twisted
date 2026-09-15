21ce2972fa38555300165a6c3b6547ad428c35db
KavyanshKhaitan2 <kavyanshkhaitan11@gmail.com>
2026-09-15 16:42:20 +0530

    Fix HTTP 500 error when trying to visit user detail page on the admin dash

--- twisted/twisted_site/views/admin/users.py
+++ twisted/twisted_site/views/admin/users.py
@@ -57,7 +57,11 @@ class UserDetailView(AdminView):
         self.audit_log.additional_context["user"] = user.profile.slack_username  # ty:ignore[unresolved-attribute] # pyright: ignore[reportAttributeAccessIssue] # pyrefly: ignore[missing-attribute]
 
         context["user"] = user
-        context["staff_perms"] = model_to_dict(user.profile.staff_permissions or {})
+        try:
+            context["staff_perms"] = model_to_dict(user.profile.staff_permissions)
+        except AttributeError:
+            context["staff_perms"] = None
+
         context["login_maybe"] = os.environ.get("LOGIN_ENABLED") == "maybe"
         return TemplateResponse(request, "admin/user.html", context)
 
