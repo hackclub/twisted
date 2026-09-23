0a473d39a8fe4e1dd200873d55d3f98b270df2d7
Lewin Kelly <heli@odex.cf>
2026-09-23 16:55:27 -0400

    Return 404 Not Found upon unknown shop regions

--- twisted/twisted_site/views/client/shop.py
+++ twisted/twisted_site/views/client/shop.py
@@ -1,5 +1,5 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 from django.views import View
 
@@ -32,7 +32,7 @@ class ShopView(View):
 
         if request.POST.get("action") == "setRegion":
             profile: Profile = as_user(request.user).profile
-            profile.region = ShopRegion.objects.get(id=request.POST["region"])
+            profile.region = get_object_or_404(ShopRegion, id=request.POST.get("region"))
             profile.save()
 
             return redirect(request.path_info)
